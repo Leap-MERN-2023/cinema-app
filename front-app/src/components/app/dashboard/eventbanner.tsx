bf1f0e6cd7b06f79120dd0b861233eef3b266653
Bilguun Gantogos <gantogos.bilguun48@gmail.com>
2024-03-14 21:49:57 +0800

    dashboard's filterSection and eventBanner

--- front-app/src/components/app/dashboard/eventbanner.tsx
+++ front-app/src/components/app/dashboard/eventbanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EventsBanner = () => {
   return (
-    <div className="w-full h-screen max-h-[32rem] p-24 relative overflow-hidden block z-10 bg-[url('https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center before:content-[''] before:absolute before:inset-0 before:block before:bg-gradient-to-t before:from-black before:opacity-90 before:z-[-5]">
+    <div className="w-full h-screen max-h-[20rem] p-24 my-10 relative overflow-hidden rounded-lg block z-10 bg-[url('https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center before:content-[''] before:absolute before:inset-0 before:block before:bg-gradient-to-t before:from-black before:opacity-90 before:z-[-5]">
       <div className="flex flex-col h-full gap-10 justify-end">
         <div className=" text-white text-4xl">
           The Adventures of Robin Hood (1938)
