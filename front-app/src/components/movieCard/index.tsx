70d6c1e63afad9bdf86e20d91b66a82a315b0782
Билгүүн Гантогос <LP230023@G301-CC0190.local>
2024-04-11 21:46:48 -0700

    movie card reverse deleted

--- front-app/src/components/movieCard/index.tsx
+++ front-app/src/components/movieCard/index.tsx
@@ -8,20 +8,17 @@ type Props = {};
 
 export const MovieCard = (props: Props) => {
   const { movies, filteredMovies, loading } = useContext(MovieContext);
-  const [pageCount, setPageCount] = useState(3);
+  const [pageCount, setPageCount] = useState(9);
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="mt-10 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
-        {filteredMovies
-          .slice(0, pageCount)
-          .map((card: any) => {
-            return loading ? (
-              <Skeleton className="w-[300px] rounded-xl bg-white bg-opacity-10 h-[450px]" />
-            ) : (
-              <MCard card={card} key={card._id} />
-            );
-          })
-          .reverse()}
+        {filteredMovies.slice(0, pageCount).map((card: any) => {
+          return loading ? (
+            <Skeleton className="w-[300px] rounded-xl bg-white bg-opacity-10 h-[450px]" />
+          ) : (
+            <MCard card={card} key={card._id} />
+          );
+        })}
       </div>
       {pageCount < filteredMovies.length && (
         <div className="mb-24 mt-8 p-3 rounded-md text-white bg-red-500 hover:bg-slate-800 duration-75">
